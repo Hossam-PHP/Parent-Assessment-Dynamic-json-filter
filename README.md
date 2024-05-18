1fbacc7dbcf5e5b9ca657c4d6c141840b80d843a
Hossam-PHP <hossamahmed548@gmail.com>
2024-05-18 22:41:00 +0300

    add project assessment files

--- README.md
+++ README.md
@@ -1,1 +1,116 @@
-# Parent-Assessment-Dynamic-json-filter
+# Dynamic JSON Filter
+
+**Author:** Hossam Rashwan
+
+**Laravel Version:** 10.2
+
+**PHP Version:** 8.1
+
+## Introduction
+
+Dynamic JSON Filter is a Laravel-based API project that enables you to filter and combine data from multiple JSON sources. This project offers a flexible way to filter and retrieve JSON data from various providers.
+
+## Features
+
+-   Combines and filters data from multiple JSON sources.
+-   Supports filtering by provider, status code, balance, and currency.
+-   Utilizes a configuration file for key mapping, facilitating adaptation to different data structures.
+-   Includes unit tests to ensure code correctness.
+
+## Installation
+
+To get started with this project, follow these steps:
+
+1. Clone the repository:
+
+    ```shell
+    git clone https://github.com/Hossam-PHP
+    ```
+
+2. Navigate to the project directory:
+
+    ```shell
+    cd Dynamic-json-filter
+    ```
+
+3. Install PHP dependencies using Composer:
+
+    ```shell
+    composer install
+    ```
+
+4. Create a `.env` file by copying `.env.example`:
+
+    ```shell
+    cp .env.example .env
+    ```
+
+5. Generate a new Laravel application key:
+
+    ```shell
+    php artisan key:generate
+    ```
+
+6. Start the Laravel development server:
+
+    ```shell
+    php artisan serve --host=0.0.0.0
+    ```
+
+    Access the application at `http://localhost:8000`.
+
+## Usage
+
+1. With the Laravel development server running, make API requests to the `/api/v1/users` endpoint to filter and retrieve JSON data. You can use query parameters to specify filters, such as `provider`, `statusCode`, `balanceMin`, `balanceMax`, and `currency`.
+
+    Example request:
+
+    ```
+    GET http://localhost:8000/api/v1/users?provider=DataProviderX&statusCode=authorised
+    ```
+
+    This request will retrieve data from `DataProviderX` with a `statusCode` of `authorised`.
+
+## Configuration
+
+-   Customize the key mapping for each data provider in the `config/keys_mapping.php` file. Adjust this file to match the structure of your JSON data sources.
+
+## Testing
+
+To run unit tests, use the following command:
+
+```shell
+php artisan test
+```
+
+## Docker Support
+
+This project includes Docker support for containerization. You can use the provided `Dockerfile` and `docker-compose.yml` files to set up and run the project in a Docker container.
+
+### Building and Running with Docker
+
+1. Clone the repository as mentioned above.
+
+2. Navigate to the project directory:
+
+    ```shell
+    cd Dynamic-json-filter
+    ```
+
+3. Build and start the Docker containers:
+
+    ```shell
+    docker-compose up -d
+    ```
+
+4. Access the application at `http://localhost:8000` in your web browser.
+
+5. When done, stop the containers:
+
+    ```shell
+    docker-compose down
+    ```
+
+## License
+
+This project is available under the [MIT License](LICENSE).
---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\FilterPipeline;
+use App\Filters\BalanceMaxFilter;
+use App\Filters\BalanceMinFilter;
+use App\Filters\CurrencyFilter;
+use App\Filters\ProviderFilter;
+use App\Filters\StatusCodeFilter;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+class UserController extends Controller
+{
+    public function index(Request $request)
+    {
+        try {
+            // Get Files
+            $combinedData = $this->getCombinedData();
+
+            // Apply filters
+            $filteredData = $this->applyFilters($request, $combinedData);
+
+            // Return Response
+            return response()->json(array_values($filteredData), 200);
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'An error occurred'], 500);
+        }
+    }
+
+    private function getCombinedData()
+    {
+        // The combined data
+        $combinedData = [];
+
+        // Get a list of JSON files in the data directory
+        $jsonFiles = Storage::disk('local')->files('json_data');
+
+        foreach ($jsonFiles as $jsonFile) {
+            $source = pathinfo($jsonFile, PATHINFO_FILENAME);
+
+            $jsonData = $this->standardizeJson($source, json_decode(Storage::disk('local')->get($jsonFile), true));
+
+            foreach ($jsonData as $user) {
+                $user['source'] = $source;
+                $combinedData[] = $user;
+            }
+        }
+
+        return $combinedData;
+    }
+
+    private function standardizeJson($fileName, $data)
+    {
+        $mapping = config("keys_mapping.$fileName", []);
+
+        // Initialize an array to store the standardized data.
+        $standardizedData = [];
+
+        // Define a mapping function to transform each item
+        $mapFunction = function ($item) use ($mapping) {
+            $standardizedItem = [];
+
+            // Map keys based on the configuration
+            array_walk($item, function ($value, $sourceKey) use ($mapping, &$standardizedItem) {
+                if ($sourceKey === 'status' && isset($mapping['status_mapping'][$value])) {
+                    $standardizedItem[$mapping['status']] = $mapping['status_mapping'][$value];
+                } elseif (isset($mapping[$sourceKey])) {
+                    $standardizedItem[$mapping[$sourceKey]] = $value;
+                }
+            });
+
+            return $standardizedItem;
+        };
+
+        // Use array_map to apply the mapping function to each item in $data
+        $standardizedData = array_map($mapFunction, $data);
+
+        return $standardizedData;
+    }
+
+    private function applyFilters(Request $request, array $data)
+    {
+        // Create a filter pipeline
+        $filterPipeline = new FilterPipeline();
+
+        if ($request->has('provider')) {
+            $filterPipeline->addFilter(new ProviderFilter());
+        }
+
+        if ($request->has('statusCode')) {
+            $filterPipeline->addFilter(new StatusCodeFilter());
+        }
+
+        if ($request->has('balanceMin')) {
+            $filterPipeline->addFilter(new BalanceMinFilter());
+        }
+
+        if ($request->has('balanceMax')) {
+            $filterPipeline->addFilter(new BalanceMaxFilter());
+        }
+
+        if ($request->has('currency')) {
+            $filterPipeline->addFilter(new CurrencyFilter());
+        }
+
+        return $filterPipeline->apply($data, $request);
+    }
+}
